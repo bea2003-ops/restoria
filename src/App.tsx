f6557250526181f0d42dedd79cda8a686e5c53b5
Odor Norbert <odor.norbert@posta.hu>
2025-03-23 00:01:16 +0100

    fix: compiler fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
 import logo from './assets/restoria-logo.jpg';
+import {Helmet} from "react-helmet-async";
 
 function App() {
 
     return (
         <div>
+            <Helmet>
+                <title>Restoria - Home</title>
+            </Helmet>
             <h1>Bea</h1>
             <img src={logo} alt="Logo"/>
         </div>
--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,12 @@ import App from './App.tsx'
 import "@fontsource/montserrat";
 import "@fontsource/montserrat/400.css";
 import "@fontsource/montserrat/700.css";
+import {HelmetProvider} from 'react-helmet-async';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App/>
+        <HelmetProvider>
+            <App/>
+        </HelmetProvider>
     </StrictMode>,
 )
