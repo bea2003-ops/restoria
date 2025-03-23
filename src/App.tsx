bc3a58f8bbd6be56c13306c3f4ac99447915dc4d
Odor Norbert <odor.norbert@posta.hu>
2025-03-23 12:26:48 +0100

    fix: localization fixes

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import logo from './assets/restoria-logo.jpg';
 import {Helmet} from "react-helmet-async";
 import Navigation from "@/components/Navigation/navigation.tsx";
 import {useTranslation} from "react-i18next";
+import i18n from "i18next";
 
 function App() {
+    document.documentElement.lang = i18n.language;
 
     const {t} = useTranslation();
-
     return (
         <div>
             <Navigation/>
