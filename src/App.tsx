df37c002e30cfc8bbea7f147ec9bed5ae35b1d40
Odor Norbert <odor.norbert@posta.hu>
2025-03-25 16:50:28 +0100

    fix: route fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 import Home from "@/pages/home/home.tsx";
 import Contact from "@/pages/contact/contact.tsx";
 import i18n from "i18next";
@@ -7,16 +7,14 @@ function App() {
     document.documentElement.lang = i18n.language;
 
     return (
-        <BrowserRouter basename="/">
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/en" element={<Home/>}/>
-                <Route path="/hu" element={<Home/>}/>
-                <Route path="/contact" element={<Contact/>}/>
-                <Route path="/en/contact" element={<Contact/>}/>
-                <Route path="/hu/contact" element={<Contact/>}/>
-            </Routes>
-        </BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/en" element={<Home/>}/>
+            <Route path="/hu" element={<Home/>}/>
+            <Route path="/contact" element={<Contact/>}/>
+            <Route path="/en/contact" element={<Contact/>}/>
+            <Route path="/hu/contact" element={<Contact/>}/>
+        </Routes>
     )
 }
 
